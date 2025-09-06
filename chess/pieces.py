3ec82ed28e6d8b8400488539b03bddf7507d677c
Blue084-end <azend11110000@gmail.com>
2025-09-06 09:23:48 +0700

    Update pieces.py

--- chess/pieces.py
+++ chess/pieces.py
@@ -1,1 +1,9 @@
+class Piece:
+    def __init__(self, name, color, position):
+        self.name = name  # Ví dụ: "Xe", "Mã"
+        self.color = color  # "red" hoặc "black"
+        self.position = position  # (row, col)
+
+    def move(self, new_position):
+        self.position = new_position
 
