6549bc5c43bd369fc200a9c7ca3543c4981c7f7b
Blue084-end <azend11110000@gmail.com>
2025-09-06 09:24:31 +0700

    Update game.py

--- chess/game.py
+++ chess/game.py
@@ -1,1 +1,19 @@
+from .board import Board
+from .pieces import Piece
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.turn = "red"
+
+    def make_move(self, from_pos, to_pos):
+        piece = self.board.get_piece(*from_pos)
+        if piece and piece.color == self.turn:
+            piece.move(to_pos)
+            self.board.set_piece(*to_pos, piece)
+            self.board.set_piece(*from_pos, None)
+            self.switch_turn()
+
+    def switch_turn(self):
+        self.turn = "black" if self.turn == "red" else "red"
 
