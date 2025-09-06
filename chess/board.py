349dd7d01cf53ea7402becfb4c453a0e8791d3a3
Blue084-end <azend11110000@gmail.com>
2025-09-06 09:23:03 +0700

    Update board.py

--- chess/board.py
+++ chess/board.py
@@ -1,1 +1,14 @@
+class Board:
+    def __init__(self):
+        self.grid = self.create_board()
+
+    def create_board(self):
+        # Tạo bàn cờ 9x10 với None ở mỗi ô
+        return [[None for _ in range(9)] for _ in range(10)]
+
+    def get_piece(self, row, col):
+        return self.grid[row][col]
+
+    def set_piece(self, row, col, piece):
+        self.grid[row][col] = piece
 
